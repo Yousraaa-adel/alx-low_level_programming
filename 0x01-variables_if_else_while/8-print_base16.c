2cf7d3cb74617f407ea6d637436c536fe2aea836
Yousra <yousraaadel@gmail.com>
2023-09-08 11:04:57 -0700

    u

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	char letter = 'a';
+	char newline = '\n';
+
+	while ((n >= 0) && (n <= 9))
+	{
+		putchar(n);
+		n++;
+	}
+
+	while ((letter >= 'a') && (letter <= 'f'))
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar(newline);
+
+	return (0);
+}
