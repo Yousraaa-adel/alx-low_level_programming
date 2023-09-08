1ca1b68e63fa6e56e6c8047a853e6aefef1b67a8
Yousra <yousraaadel@gmail.com>
2023-09-08 08:26:01 -0700

    upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+	char newline = '\n';
+
+	while((lower >= 'a') && (lower >= 'z'))
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	while((upper >= 'a') && (upper <= 'z'))
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar(newline);
+
+	return (0);
+}
