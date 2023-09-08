1c0fc16d6008a2680e2a69b41e83e1fe9d9b56bf
Yousra <yousraaadel@gmail.com>
2023-09-08 09:42:34 -0700

    Trying

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while ((n > 0) && (n <= 9))
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
