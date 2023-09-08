3c38ae64ae582baa8f3defcaed0d9019743f3bb8
Yousra <yousraaadel@gmail.com>
2023-09-08 13:53:52 -0700

    t

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstn = 0;
+	int secn = 0;
+
+	while ((firstn >= 0) && (firstn <= 9))
+	{
+		putchar(firstn + 48);
+		while ((secn >= 0) && (secn <= 9))
+		{
+			putchar(secn + 48);
+			while (secn <= 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			secn++;
+		}
+		firstn++;
+	}
+	putchar(10);
+
+	return (0);
+}
