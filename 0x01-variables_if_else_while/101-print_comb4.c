6339fc0180b8fe1f981d9869078b94ebe682e3fd
Yousra <yousraaadel@gmail.com>
2023-09-09 06:19:52 -0700

    t

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,29 +11,26 @@ int main(void)
 	int num2 = '0';
 	int num3 = '0';
 
-	while (num1 <= 9)
+	while (num1 <= '9')
 	{
-		while (num2 <= 9)
+		while (num2 <= '9')
 		{
-			while (num3 <= 9)
+			while (num3 <= '9')
 			{
-				if(!(num1 > num2 || num1 == num2))
+				if (!(num1 > num2 || num1 == num2) && !(num2 > num3 || num2 == num3))
 				{
-					if (!(num2 > num3 || num2 == num3))
+					if (!(num1 > num3 || num1 == num3))
 					{
-						if (!(num1 > num3 || num1 == num3))
+						putchar(num1);
+						putchar(num2);
+						putchar(num3);
+						if (num1 == '7' && num2 == '8' && num3 == '9')
 						{
-							putchar(num1);
-							putchar(num2);
-							putchar(num3);
-							if (num1 == '7' && num2 == '8' && num3 == '9')
-							{
-								putchar('\n');
-							} else
-							{
-								putchar(',');
-								putchar(' ');
-							}
+							putchar('\n');
+						} else
+						{
+							putchar(',');
+							putchar(' ');
 						}
 					}
 				}
