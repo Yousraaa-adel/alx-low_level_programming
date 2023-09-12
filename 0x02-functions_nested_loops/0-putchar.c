3a734f048f46911bd967c341060b5008d26d86b1
Yousra <yousraaadel@gmail.com>
2023-09-12 07:20:26 -0700

    b

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+void print_putchar(char *string) 
+{
+  putchar(*string);
+}
+
 int main(void)
 {
 	char *string = "_putchar";
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void print_putchar(char *string);
-#endif /* MAIN_H */
