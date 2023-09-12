2df439c83d7a03cc3552d09e749b2d6f51ea538e
Yousra <yousraaadel@gmail.com>
2023-09-12 09:12:38 -0700

    h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_alphabet_x10: Printing 10 times the alphabet.
+ */
+void print_alphabet_x10(void)
+{
+	int letter = 'a';
+	int line = 0;
+
+	while (line < 10)
+	{
+		if (letter == 'z')
+		{
+		putchar('\n');
+		}
+		else
+		{
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+		}
+	}
+}
