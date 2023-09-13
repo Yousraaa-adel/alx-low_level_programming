0a0f9a2e801dfc76199a851ed93916e5800825aa
Yousra <yousraaadel@gmail.com>
2023-09-13 09:29:56 -0700

    u

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,6 +2,36 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * Return:
+ * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int count = 0;
+	unsigned long fib1 = 0;
+	unsigned long fib2 = 1;
+	unsigned long sum;
+
+	while (count < 50)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+
+		count++;
+	}
+
+	return (0);
+}
