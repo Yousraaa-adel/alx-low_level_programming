aa023fa8d3deffcda91598441300e9cea382fa5b
Yousra <yousraaadel@gmail.com>
2023-09-14 06:27:28 -0700

    SO?

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - A function to multiplies two digits
+ * @a: any integer
+ * @b: any integer
+ * Return: the prod of the two integers
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
