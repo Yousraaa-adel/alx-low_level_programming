c067546d9ddc5f8da8ab3d5e56c98cd7196c3bfc
Yousra <yousraaadel@gmail.com>
2023-09-14 10:52:43 -0700

    FIRST TRY!!!!!!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+		num++;
+	}
+
+	return (0);
+}
