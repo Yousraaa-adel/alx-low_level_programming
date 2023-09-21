b41a4bec1d0783614c076b6a3cb613eb3f2413dc
Yousra <yousraaadel@gmail.com>
2023-09-21 12:18:10 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings together
+ * @s1: any string
+ * @s2: any string
+ * Return: >0 if greater, <0 if less, 0<F10> if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int result = 0;
+
+	while ((s1[i] != '\0') && (s2[j] != '\0'))
+	{
+		if (s1[i] < s2[j])
+		{
+			result = 15;
+		}
+		else if (s1[i] > s2[j])
+		{
+			result = -15;
+		}
+		else
+		{
+			result = 0;
+		}
+
+		i++;
+		j++;
+	}
+	return (result);
+}
