b3201ef9824eb596b07739c5a8229ccb2f4436bd
Yousra <yousraaadel@gmail.com>
2023-10-01 11:56:23 -0700

    So?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * check_p - A function that checks for a prime number
+ * @o: an integer
+ * @l: an integer
+ * Return: 0 if is divisable, and 1 if not
+ */
 int check_p(int o, int l)
 {
 	if (o <= 1 || (o != l && o % l == 0))
@@ -11,10 +17,15 @@ int check_p(int o, int l)
 		return (1);
 	}
 
-	return check_p(o, l + 1);
+	return (check_p(o, l + 1));
 }
 
+/**
+ * is_prime_number - A function that checks for prime n.
+ * @n: any integer
+ * Return: 0 if not a prime n, 1 if prime
+ */
 int is_prime_number(int n)
 {
-	return check_p(n, 2);
+	return (check_p(n, 2));
 }
