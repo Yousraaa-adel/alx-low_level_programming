b2fb625e17d5b870ca6d3ef7d1c0428be4718f6d
Yousra <yousraaadel@gmail.com>
2023-10-02 10:42:59 -0700

    Done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc);
+	}
+
+	return (0);
+}
