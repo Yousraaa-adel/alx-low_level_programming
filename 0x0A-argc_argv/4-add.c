7ad69dabcec1ec23c4d68db3a25ea232752b01ac
Yousra <yousraaadel@gmail.com>
2023-10-05 12:57:49 -0700

    b

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,49 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- *
- *
- *
+ * isInteger - A function that checks if s is an integer
+ * @s: a string to check
+ * Return: 0 if there's no number or 1 if one of
+ * the numbers contains symobls.
  */
-int main(int argc, char *argv[])
+
+int isInt(const char *s)
 {
-	int i = 0;
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		if (s[x] < '0' || s[x] > '9')
+		{
+			return (1);
+		}
+		x++;
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point
+ * @argc: an integer
+ * @argv: an array of a list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
 	int sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
+	while (--argc)
 
-	while (i < argc)
 	{
-		int j = 0;
-		while (j < argv[i][j] != '\0')
+		if (isInt(argv[argc]))
 		{
-			if (!isdigit((unsigned char)argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
+			printf("Error\n");
+			return (1);
 		}
-
-		sum += atoi(argv[i]);
-		i++;
+		sum += atoi(argv[argc]);
 	}
-
-	printf("%d\n", sum);
+	printf("%i\n", sum);
 	return (0);
 }
