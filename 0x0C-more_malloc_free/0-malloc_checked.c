fbe2d5f65f3adab2bb6f9e848adb7dd81ee01e7b
Yousra <yousraaadel@gmail.com>
2023-10-08 12:30:55 -0700

    g

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,13 @@
 
 /**
  * malloc_checked - A function that allocates memory using malloc.
- *
- *
+ * @b: an integer
+ * Return: a pointer for the allocated memory and 98 if failed
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-       
+
 	ptr = malloc(b);
 
 	if (ptr == NULL)
