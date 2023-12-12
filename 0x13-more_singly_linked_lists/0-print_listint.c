398e78310892635ae79991e747e1754abbdf4233
Yousraaadel <yousraaadel@gmail.com>
2023-12-12 17:28:39 +0200

    singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - prints content of the string
- * @h: singly linked list
- * Return: prints elements of the list
- */
+* print_listint - prints content of the string
+* @h: singly linked list
+* Return: prints elements of the list
+*/
 size_t print_listint(const listint_t *h)
 {
-	int a;
+    int i = 0;
 
-	a = 0;
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		a++;
-	}
-
-	return (a);
-
+    return (i);
 }
