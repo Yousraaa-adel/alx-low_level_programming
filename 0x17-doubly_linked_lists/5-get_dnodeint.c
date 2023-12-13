eb40142639fe45d71d419b5278ff5c9722c385fb
Yousraaadel <yousraaadel@gmail.com>
2023-12-13 17:29:51 +0200

    5th task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+* @head: a pointer to the doubly linked list
+* @index: unsigned integer, index to the required node
+* Return: the nth node of a the list, and NULL if it doesn't exist
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+unsigned int count = 0;
+
+/*Check if list is empty or index doesn't exist*/
+if (head == NULL || index == NULL)
+{
+return (NULL);
+}
+
+while (head)
+{
+if (count == index)
+{
+return (head);
+}
+
+head = head->next;
+count++;
+
+}
+
+return (head);
+
+}
