e13b37f86eea64e4e0f6ebfa67a6c724387ac830
Yousraaadel <yousraaadel@gmail.com>
2023-12-13 17:58:42 +0200

    7th task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 
-dlistint_t *new_node, *temp = *head;
+dlistint_t *new_node, *temp = *h;
 unsigned int i = 0;
 
 if (*h == NULL)
@@ -32,7 +32,7 @@ if (idx == 0)
 {
 new_node->next = temp;
 new_node->prev = NULL;
-*head = new_node;
+*h = new_node;
 return (new_node);
 }
 for (; i < (idx - 1); i++)
